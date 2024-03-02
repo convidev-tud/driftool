20674924a0907f51a5ddc472d0c2ed31ffc16ae9
Karl Kegel <private@kkegel.de>
2024-03-02 20:52:47 +0100

    Add docker doc

--- deb_run.sh
+++ deb_run.sh
@@ -13,6 +13,9 @@ echo the user is: "$USER"
 echo create ramdisk with $2 GB size
 mkdir -p ./tmp
 sudo chmod 777 ./
+
+# Comment the following two lines to disable the ramdisk creation.
+# This reduces the RAM usage but increases the analysis time.
 sudo mount -v -t tmpfs -o size=$(($2))G ramdisk ./tmp
 mount | tail -n 1
 
