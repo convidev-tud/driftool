de882d2732b7f0f23449c8165063d054e6a257af
Karl Kegel <private@kkegel.de>
2025-01-01 21:53:35 +0100

    Fix multithred test setup

--- driftool_kt/systest/testrun_multi_no_file_actions.sh
+++ driftool_kt/systest/testrun_multi_no_file_actions.sh
@@ -7,6 +7,4 @@
 # $4 is the relative path to the repository within the input directory.
 # $5 is the relative path to the output directory within the input directory.
 
-cd ..
-echo "gradle run ${PWD}/$1 ${PWD}/$2 $3 ${PWD}/../ -i $4 -o $5 -m git -t 4"
-gradle run --args="${PWD}/$1 ${PWD}/$2 $3 ${PWD}/../ -i $4 -o $5 -m git -t 4"
+./localrun_multithread.sh ../volume ../tmp example_config.yaml conflict-example ./
